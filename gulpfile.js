74ed6e91b53d80c993c5f87c010699a8eca30d4b
psdcon <psdcon@gmail.com>
2014-08-30 20:32:01 +0100

    First commit

---
+++ gulpfile.js
@@ -0,0 +1,59 @@
+var gulp = require('gulp'),
+    gutil = require('gulp-util'),
+    compass = require('gulp-compass'),
+    connect = require('gulp-connect'),
+    uglify = require('gulp-uglify'),
+    gulpif = require('gulp-if'),
+    minifyHTML = require('gulp-minify-html');
+
+var env,
+    sassSources,
+    htmlSources,
+    outputDir,
+    sassStyle;
+
+env = process.env.NODE_ENV || 'development';
+
+if (env==='development') {
+  outputDir = 'builds/development/';
+  sassStyle = 'expanded';
+} else {
+  outputDir = 'builds/production/';
+  sassStyle = 'compressed';
+}
+
+sassSources = ['components/sass/style.scss'];
+htmlSources = [outputDir + '*.html'];
+
+gulp.task('compass', function() {
+  gulp.src(sassSources)
+    .pipe(compass({
+      sass: 'components/sass',
+      image: outputDir + 'images',
+      style: sassStyle
+    })
+    .on('error', gutil.log))
+    .pipe(gulp.dest(outputDir + 'css'))
+    .pipe(connect.reload());
+});
+
+gulp.task('watch', function() {
+  gulp.watch('components/sass/*.scss', ['compass']);
+  gulp.watch('builds/development/*.html', ['html']);
+});
+
+gulp.task('connect', function() {
+  connect.server({
+    root: outputDir,
+    livereload: true
+  });
+});
+
+gulp.task('html', function() {
+  gulp.src('builds/development/*.html')
+    .pipe(gulpif(env === 'production', minifyHTML()))
+    .pipe(gulpif(env === 'production', gulp.dest(outputDir)))
+    .pipe(connect.reload());
+});
+
+gulp.task('default', ['html', 'compass', 'connect', 'watch']);
