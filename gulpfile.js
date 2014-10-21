c3766c004308c13796b8ea2d34ec831b04164840
psdcon <psdcon@gmail.com>
2014-10-21 16:48:22 +0800

    first commit

--- gulpfile.js
+++ gulpfile.js
@@ -1,60 +1,37 @@
 var gulp = require('gulp'),
     gutil = require('gulp-util'),
-    compass = require('gulp-compass'),
     connect = require('gulp-connect'),
-    uglify = require('gulp-uglify'),
-    gulpif = require('gulp-if'),
-    prefix = require('gulp-autoprefixer'),
-    minifyHTML = require('gulp-minify-html');
+    compass = require('gulp-compass'),
+    prefix = require('gulp-autoprefixer');
 
-var env,
-    sassSources,
-    htmlSources,
-    outputDir,
-    sassStyle;
-
-env = process.env.NODE_ENV || 'development';
-
-if (env==='development') {
-  outputDir = '/';
-  sassStyle = 'expanded';
-} else {
-  outputDir = 'production/';
-  sassStyle = 'compressed';
-}
-
-sassSources = ['sass/style.scss'];
-htmlSources = [outputDir + '*.html'];
-
-gulp.task('compass', function() {
-  gulp.src(sassSources)
-    .pipe(compass({
-      sass: 'sass',
-      image: outputDir + 'images',
-      style: sassStyle
-    })
-    .on('error', gutil.log))
-    .pipe(prefix())
-    .pipe(gulp.dest(outputDir + 'css'))
-    .pipe(connect.reload());
-});
 
 gulp.task('watch', function() {
-  gulp.watch('sass/*.scss', ['compass']);
+  gulp.watch('css/*.scss', ['compass']);
   gulp.watch('*.html', ['html']);
 });
 
 gulp.task('connect', function() {
   connect.server({
-    root: outputDir,
+    root: '',
     livereload: true
   });
 });
 
 gulp.task('html', function() {
   gulp.src('*.html')
-    .pipe(gulpif(env === 'production', minifyHTML()))
-    .pipe(gulpif(env === 'production', gulp.dest(outputDir)))
+    .pipe(connect.reload());
+});
+
+gulp.task('compass', function() {
+  gulp.src('css/*.scss')
+    .pipe(compass({
+      sass: 'css',
+      image: 'assets',
+      style: 'expanded'
+    })
+    .on('error', gutil.log))
+    .pipe(prefix())
+    .pipe(gulp.dest('css'))
     .pipe(connect.reload());
 });
 
